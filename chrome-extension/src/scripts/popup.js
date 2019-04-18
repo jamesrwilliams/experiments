95d876748c97c1a3f9f8b1c3d8ce17ef8a0e6770
James Williams <james@jamesrwilliams.co.uk>
2019-04-17 22:53:23 -0400

    feat: Chrome extension - Added syntax highlighting and basic clipboard usage

--- chrome-extension/src/scripts/popup.js
+++ chrome-extension/src/scripts/popup.js
@@ -1,18 +1,58 @@
-console.log('Hello from popup.js');
+/**
+ * popup.js
+ */
+
+const editor = ace.edit("editor");
+    editor.setOptions({
+        theme: 'ace/theme/monokai',
+        mode: 'ace/mode/html',
+    });
+
+const result = null;
+const textarea = document.getElementById('clip');
+const use_clipboard_btn = document.getElementById('use_clipboard');
+
+use_clipboard_btn.addEventListener('click', () => {
+
+    const clipboard = getClipboard();
+
+    if(clipboard !== '' ) {
+        sendExtensionMessage({
+            element: target.value,
+            content: clipboard
+        });
+    }
+
+});
 
 document.getElementById('send_message').addEventListener('click', () => {
 
-    const targetElement = document.getElementById('target');
-    const replacementHTML = document.getElementById('replacement');
+    const target = document.getElementById('target');
 
-    sendExtentionMessage({
-        element: targetElement.value,
-        content: replacementHTML.value
-    })
+    sendExtensionMessage({
+        element: target.value,
+        content: editor.getValue()
+    });
 });
 
-function sendExtentionMessage(message) {
+function sendExtensionMessage(message) {
     chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
         chrome.tabs.sendMessage(tabs[0].id, message);
     });
+}
+
+function getClipboard() {
+    let result = null;
+    const textarea = document.getElementById('clip');
+          textarea.value = '';
+          textarea.select();
+
+    if (document.execCommand('paste')) {
+        result = textarea.value;
+    } else {
+        console.error('failed to get clipboard content');
+    }
+
+    textarea.value = '';
+    return result;
 }
