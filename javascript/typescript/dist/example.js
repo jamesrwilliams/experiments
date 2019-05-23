84534ccb2ebcaf15aaad0f8440761a4cd8dd6bca
James Williams <james@jamesrwilliams.co.uk>
2019-05-23 07:19:12 -0400

    feat: Added basic webpack and typescript demos

---
+++ javascript/typescript/dist/example.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ExampleClass = /** @class */ (function () {
+    function ExampleClass() {
+    }
+    return ExampleClass;
+}());
+exports.ExampleClass = ExampleClass;
