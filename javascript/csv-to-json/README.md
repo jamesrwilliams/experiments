21182fe6b73d86f31c8ae366f3c73e67113780dd
James Williams <james@jamesrwilliams.co.uk>
2019-08-19 11:29:45 -0400

    feat: CSV-2-JSON in NodeJS for Kickflip.

---
+++ javascript/csv-to-json/README.md
@@ -0,0 +1,5 @@
+# CSV-2-JSON
+
+A node script for transposing a CSV file of translations to a custom JSON structure. Where tokens are organised by language and tokens. 
+
+
---
+++ javascript/csv-to-json/index.js
@@ -0,0 +1,72 @@
+const fs = require('fs-extra');
+const csv = require('csv-parser');
+const results = [];
+
+fs.createReadStream('data/two-tokens.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+        generateOutput(results);
+    });
+
+function generateOutput(input) {
+
+    const translations = input;
+    let count = 0;
+    let output = [];
+
+    translations.forEach((translation) => {
+
+        const lng = translation.Lng;
+        const token = translation.Token;
+        const value = translation.Value;
+
+        let indexInArray = objectArrayIndex(output, lng, 'identifier');
+
+        if(!indexInArray) {
+            output.push({
+                "identifier": `${lng}`,
+                "prefix": `${lng}-`,
+                "tokens": [
+                    [token, value]
+                ]
+            })
+        } else {
+            output[indexInArray].tokens.push([token, value]);
+        }
+
+        count++;
+    });
+
+    fs.writeFile('tokens.json', JSON.stringify(output, null, 4), (err) => {
+        // throws an error, you could also catch it here
+        if (err) throw err;
+
+        // success case, the file was saved
+        console.log('Output saved!');
+    });
+
+}
+
+/**
+ * [objectArrayIndex description]
+ * @param  {array} haystack [description]
+ * @param  {string} needleValue  [description]
+ * @param  {string} needleProperty  [description]
+ * @return {int | boolean}  [description]
+ */
+function objectArrayIndex(haystack, needleValue, needleProperty){
+
+    let index = false;
+    for(let i = 0; i < haystack.length; i++) {
+
+        if (haystack[i][needleProperty] === needleValue) {
+
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+
+}
