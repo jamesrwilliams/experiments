3a0a2fa7f063641832ea1b42ab9147111a8179f3
James Williams <james@jamesrwilliams.co.uk>
2019-12-03 07:19:39 -0500

    feat: Started using fakes.

--- javascript/mocha-sinon-chai/tests/index.spec.js
+++ javascript/mocha-sinon-chai/tests/index.spec.js
@@ -1,6 +1,7 @@
 const helpers = require('../src/helpers');
 const chai = require('chai');
 const sinon = require('sinon');
+const fake = require('sinon').fake;
 
 const expect = chai.expect;
 const assert = chai.assert;
@@ -8,12 +9,17 @@ const assert = chai.assert;
 describe('Helpers', function() {
    describe('#logger()', function() {
        it('should ensure helper is called once', function() {
+
+           // Create Sinon Fake
+           // Replace real helper() with fake using sinon.replace();
+           const fakeHelper = sinon.fake.returns('apple pie');
+           sinon.replace(helpers, 'logger', fakeHelper);
+
            // Ensure called helpers.helper() once.
            helpers.helper();
            // helpers.logger() should be spied
 
-           assert(callback.calledWith(1, 2, 3));
-           expect(true).to.be.true;
+           console.log(fakeHelper.callCount);
        });
    });
 
